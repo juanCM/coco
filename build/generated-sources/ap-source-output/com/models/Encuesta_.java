41ff3ac633c9d62ebfe3eb60846af6a6c1834a0f
Juan Carvajal <juan98.cm@gmail.com>
2017-06-04 16:29:03 -0500

    Merge

---
+++ build/generated-sources/ap-source-output/com/models/Encuesta_.java
@@ -0,0 +1,18 @@
+package com.models;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-04T16:13:47")
+@StaticMetamodel(Encuesta.class)
+public class Encuesta_ { 
+
+    public static volatile SingularAttribute<Encuesta, String> descripcion;
+    public static volatile SingularAttribute<Encuesta, Integer> num_preguntas;
+    public static volatile SingularAttribute<Encuesta, Integer> visible;
+    public static volatile SingularAttribute<Encuesta, Integer> id_usuario;
+    public static volatile SingularAttribute<Encuesta, String> titulo;
+    public static volatile SingularAttribute<Encuesta, Integer> id;
+
+}
---
+++ build/generated-sources/ap-source-output/com/models/Opcion_.java
@@ -0,0 +1,15 @@
+package com.models;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-04T16:13:47")
+@StaticMetamodel(Opcion.class)
+public class Opcion_ { 
+
+    public static volatile SingularAttribute<Opcion, String> opcion;
+    public static volatile SingularAttribute<Opcion, Integer> id_pregunta;
+    public static volatile SingularAttribute<Opcion, Integer> id;
+
+}
