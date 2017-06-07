f448e4b6bb14e98946c7d693130423b7a9a6bf89
DESKTOP-6TRD68D\Petite <yunilawliiet@gmail.com>
2017-06-06 21:39:46 -0500

    Interfaz actualizada

---
+++ build/generated-sources/ap-source-output/com/models/Respuesta_.java
@@ -0,0 +1,16 @@
+package com.models;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-06T20:55:16")
+@StaticMetamodel(Respuesta.class)
+public class Respuesta_ { 
+
+    public static volatile SingularAttribute<Respuesta, Integer> id_pregunta;
+    public static volatile SingularAttribute<Respuesta, Integer> id;
+    public static volatile SingularAttribute<Respuesta, Integer> id_contestadas;
+    public static volatile SingularAttribute<Respuesta, String> respuesta;
+
+}
